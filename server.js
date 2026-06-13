4241ac6aa2681a52463ac9104dabd6337dbf967b
RIYAWAGHELA <riyawaghela076@gmail.com>
2026-06-13 08:56:13 +0530

    Added Render PORT config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('./database/db.js');
 const ticketRoutes = require('./routes/ticketRoutes.js');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
